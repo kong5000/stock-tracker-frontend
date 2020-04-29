959d9aec18bf85051b0d796acf19d09dfc5f1568
Keith <keith.ong5000@gmail.com>
2020-04-28 21:53:49 -0700

    Restyle SideBar with flexbox to spread content evenly

--- src/components/Navigation/Sidebar/Sidebar.js
+++ src/components/Navigation/Sidebar/Sidebar.js
@@ -51,46 +51,46 @@ const Sidebar = (props) => {
                 closeDrawer={closeDrawer}
             />
             <BackDrop
-                show={showDrawer} 
+                show={showDrawer}
                 onClick={onMenuClicked}
-                />
+            />
             <div className='sidebar'>
                 <div>
-                    <h1 className="logo">BeanS
-                <i className="fas fa-seedling fa-1x icon"></i>
-                        ock
-            </h1>
+                    <h1 className="logo">BeanS<i className="fas fa-seedling fa-1x icon"></i>ock
+                     </h1>
                 </div>
-                <div className='actions'>
-                    <FontButton type="order" onClick={props.onOrderClicked} />
-                    <FontButton type="pie" onClick={props.onAllocationClicked} />
-                    <FontButton type="cash" onClick={props.onCashClicked} />
-                    <FontButton type="gear" onClick={props.onSettingsClicked} />
-                </div>
-                <div className="scrollable">
-                    <div className="cash-display">
-                        <div>Total Cash</div>
-                        <div className="cash">${Number(props.cash).toFixed(2)}</div>
-                        <hr />
-                    </div>
-                    <div className="profit-display">
-                        <div>Total Profit</div>
-                        <div className="cash">${Number(props.profit).toFixed(2)}</div>
-                        <hr />
-                    </div>
-                    <div className="latest-update-indicator">
-                        Last Update
+                {/* <div className="side-bar-content"> */}
+                    {/* <div className='actions'> */}
+                        <FontButton type="order" onClick={props.onOrderClicked} />
+                        <FontButton type="pie" onClick={props.onAllocationClicked} />
+                        <FontButton type="cash" onClick={props.onCashClicked} />
+                        <FontButton type="gear" onClick={props.onSettingsClicked} />
+                    {/* </div> */}
+                    <div className="scrollable">
+                        <div className="cash-display">
+                            <div>Total Cash</div>
+                            <div className="cash">${Number(props.cash).toFixed(2)}</div>
+                            <hr />
+                        </div>
+                        <div className="profit-display">
+                            <div>Total Profit</div>
+                            <div className="cash">${Number(props.profit).toFixed(2)}</div>
+                            <hr />
+                        </div>
+                        <div className="latest-update-indicator">
+                            Last Update
                     <div>{formatDate(props.lastUpdate)}</div>
+                        </div>
                     </div>
+                    <div className="side-bar-logout">
+                        <Link onClick={logoutHandler} to='/' href="#projects-section" className="sidebar-link">
+                            <FontButton type="signout" />
+                        </Link>
+                    </div>
+                    <hr />
                 </div>
-                <div className="side-bar-logout">
-                    <Link onClick={logoutHandler} to='/' href="#projects-section" className="sidebar-link">
-                        <FontButton type="signout" />
-                    </Link>
-                </div>
-                <hr />
             </div>
-        </div>
+        // </div>
     )
 }
 
